892362b8a5e4407d4cc414a95c9beea298694310
kimminseo <gimg51747gmail.com>
2023-07-14 16:02:18 +0900

    계산기 기록

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-var num =0 ;
-var symbol ;
-
-function btn_click (a) {
-    resultFld.value += a;
-}
-
-function cal(b) {
-    var firstNum = (document.getElementById("resultFld").value);
-    num = firstNum;
-    symbol = b ;
-    return document.getElementById("resultFld").value = "";
-}
-
-function compute () {
-    var secNum = parseFloat(document.getElementById("resultFld").value);
-    var result ;
-    switch (symbol) {
-        case '+' : result = Number(num) + Number(secNum) ; 
-        document.getElementById("resultFld").value = result; 
-        break;
-        case '-' : result = Number(num) - Number(secNum) ; 
-        document.getElementById("resultFld").value = result;
-        break;
-        case '*' : result = Number(num) * Number(secNum) ; 
-        document.getElementById("resultFld").value = result;
-        break;
-        case '/' : result = Number(num) / Number(secNum) ; 
-        document.getElementById("resultFld").value = result;
-        break;
-    }        
-}
-
-function clear_del() {
-    document.getElementById("resultFld").value = "";
-    num = 0;
-}
+var calculator = {
+    num: 0,
+    symbol: '',
+    history: [],
+    
+    btn_click: function(a) {
+      document.getElementById("resultFld").value += a;
+    },
+    
+    cal: function(b) {
+      var firstNum = document.getElementById("resultFld").value;
+      this.num = firstNum;
+      this.symbol = b;
+      document.getElementById("resultFld").value = "";
+    },
+    
+    compute: function() {
+      var secNum = parseFloat(document.getElementById("resultFld").value);
+      var result;
+      
+      switch (this.symbol) {
+        case '+':
+          result = Number(this.num) + Number(secNum);
+          break;
+        case '-':
+          result = Number(this.num) - Number(secNum);
+          break;
+        case '*':
+          result = Number(this.num) * Number(secNum);
+          break;
+        case '/':
+          result = Number(this.num) / Number(secNum);
+          break;
+      }
+      
+      document.getElementById("resultFld").value = result;
+      this.history.push(this.num + ' ' + this.symbol + ' ' + secNum + ' = ' + result);
+    },
+    
+    clear_del: function() {
+      document.getElementById("resultFld").value = "";
+      this.num = 0;
+    },
+    
+    getHistory: function() {
+      return this.history;
+    }
+  };
